8becfc6b4a84dd1b88018a1c602a900b785e7fef
asadovenko <artem.sadovenko.it.2021@lpnu.ua>
2023-03-29 11:07:56 +0300

    added env vars, tables moduls

--- lab1/main.tf
+++ lab1/main.tf
@@ -1,11 +1,24 @@
 module "authors_table" {
-    source = "../modules/dynamodb"
+  source = "../modules/dynamodb"
 
-    table_name = "Authors"
+  table_name = "Authors"
 }
 
-module "picture_table" {
-    source = "../modules/dynamodb"
-    
-    table_name = "Pictures"
+module "courses_table" {
+  source = "../modules/dynamodb"
+
+  table_name = "Courses"
+}
+
+module "lambda_api" {
+  source = "../modules/lambda-api"
+
+  function_name = module.naming.id
+  handler       = "index.handler"
+  lambda_zip    = "${path.module}../lambda-api.zip"
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
