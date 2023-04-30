073700a68f64d7b577b9c16831fe7c0f89e80e85
Artem <asadovenko7@gmail.com>
2023-05-01 02:05:00 +0300

    added lampda module(main, vars)

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -11,3 +11,13 @@ variable "handler" {
   default     = ""
   description = "Function entrypoint in your code."
 }  
+
+variable "context"{
+  description = "Label module context"
+}
+
+variable "env_var"{
+  default ={}
+  description = "Map of environment variables that are accessible from the function code during execution. If provided at least one key must be present."
+  type = map(string)
+}
